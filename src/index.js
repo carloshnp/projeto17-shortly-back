2bf81d7c46d5718fabed2e08cce7b8ef15d22b9f
carloshnp <carloshnpwork@gmail.com>
2022-12-20 01:46:10 -0300

    feat: API structure

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log("Server running in port: " + port));

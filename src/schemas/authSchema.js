b5918e122bc6de10443d2656ced98892bc744ee4
carloshnp <carloshnpwork@gmail.com>
2022-12-23 01:48:38 -0300

    feat: login schema

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -6,3 +6,8 @@ export const userSchema = joi.object({
     password: joi.string().required(),
     confirmPassword: joi.string().required()
 })
+
+export const loginSchema = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(3).required()
+})

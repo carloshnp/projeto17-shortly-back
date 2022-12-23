68e5ac023c2bc3735d341f3bb2297eb16bf1ffad
carloshnp <carloshnpwork@gmail.com>
2022-12-23 00:51:46 -0300

    feat: authRouter on app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import authRouter from './routes/authRoutes.js'
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(authRouter);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log("Server running in port: " + port));
